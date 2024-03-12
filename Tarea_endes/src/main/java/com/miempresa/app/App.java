675959951b7dcc13452ca7b6a3c57fc23b159811
David Montes Diaz <dmondia679@g.educaand.es>
2024-03-12 21:18:17 +0100

    Añadido la resta

--- Tarea_endes/src/main/java/com/miempresa/app/App.java
+++ Tarea_endes/src/main/java/com/miempresa/app/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int resta=a-b;
+        System.out.println("La resta de: " + a + " y " + b + " es: " + resta);
     }
 }
