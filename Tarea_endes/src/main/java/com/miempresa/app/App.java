74dfba83ba0a4a8ece597c082c8712dc2afdcffc
David Montes Diaz <dmondia679@g.educaand.es>
2024-03-12 21:19:20 +0100

    solucionado un problema

--- Tarea_endes/src/main/java/com/miempresa/app/App.java
+++ Tarea_endes/src/main/java/com/miempresa/app/App.java
@@ -9,7 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        int resta=a-b;
-        System.out.println("La resta de: " + a + " y " + b + " es: " + resta);
     }
 }
