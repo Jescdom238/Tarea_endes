9e1c02d4672f19eca3bed5eef01f69a0553a749a
Jescdom238 <jescdom238@g.educaand.es>
2024-03-12 21:13:17 +0100

    Añadida la suma

--- Tarea_endes/src/main/java/com/miempresa/app/App.java
+++ Tarea_endes/src/main/java/com/miempresa/app/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+       int a=1;
+       int b=2;
+       int suma=a+b;
+       System.out.Print("La suma de " +a +" y "+b+" es: "+suma)
     }
 }
